90a16ef095fa1dfc0676d50ed1de3e2e91204fd5
Muhammad-Manzoor <muhammadmanzoor2302@gmail.com>
2024-09-14 19:54:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+function toggleMenu() {
+    let elements = document.getElementsByClassName('side');
+    let elementsArray = Array.from(elements);
+    var sidebar = document.getElementById("sidebar");
+    
+    sidebar.classList.toggle("collapsed");
+    
+    elementsArray.forEach(function(element) {
+        
+        
+        if (element.style.display === 'block') {
+            element.parentElement.style.width = 'inherit';
+            element.parentElement.style.marginLeft = '140px'; 
+            element.style.display = 'none';
+        } else {
+            element.parentElement.style.width = '100%';
+            element.parentElement.style.marginLeft = '-270px';
+            setTimeout(() => {
+                element.style.display = 'block';
+            }, 0600);
+        }
+    });
+}
